25f88b18cb7f9ccdd7ff587e371bd04af79f19e6
Esdras Pavon <pavonesdras@gmail.com>
2018-12-21 12:14:04 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import { BrowserRouter, Route, Link } from "react-router-dom"
+import { Router, Route, Link } from "react-router-dom"
+import Home from './pages/Home'
+import Recipe from './pages/Recipe'
+import Timer from './pages/Timer'
+import './App.css'
 
-class App extends Component {
+import { createBrowserHistory } from 'history';
+import ReactGA, { pageview } from 'react-ga';
+import IfOffline from './components/IfOffline';
+
+const history = createBrowserHistory()
+
+ReactGA.initialize('UA-000000-01')
+ReactGA-pageview(window.location.pathname + window.location.search)
+
+history.listen(function(location) {
+  ReactGA-pageview(window.location.pathname + window.location.search)
+})
+
+
+export default class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      // <BrowserRouter>
+      <Router history={history}>
+        <div>
+          <header>
+            <Link to="/">Recetas <IfOffline>Offline</IfOffline>  </Link>
+            <Link to='/timer' className="timerLink">Timer</Link>
+          </header>
+
+          <main>
+            <Route exact path="/" component={Home} />
+            <Route path="/recipe/:recipeId" component={Recipe} />
+            <Route path="/timer" component={Timer} />
+          </main>
+        </div>
+      {/* </BrowserRouter> */}
+      </Router>
     );
   }
 }
-
-export default App;
